0584718b4b6aa55c16f42df0fb0dbf2cdda3701f
Alban Bailly <alban@zivtech.com>
2016-06-12 15:16:18 -0700

    layout, header and nav theming

--- themes/custom/plbear/js/scripts.js
+++ themes/custom/plbear/js/scripts.js
@@ -1,1 +1,58 @@
-'use strict';
+/**
+ * @file
+ * A JavaScript file for the theme.
+ *
+ * In order for this JavaScript to be loaded on pages, see the instructions in
+ * the README.txt next to this file.
+ */
+
+// JavaScript should be made compatible with libraries other than jQuery by
+// wrapping it with an "anonymous closure". See:
+// - http://drupal.org/node/1446420
+// - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+(function ($, Drupal, window, document, undefined) {
+
+  'use strict';
+
+  //Use the sample behavior pattern below
+
+  Drupal.behaviors.bearCoat = {
+   attach: function (context, settings) {
+     context = context || document;
+     settings = settings || Drupal.settings;
+
+
+      var lastScrollTop = 0,
+          delta = 5,
+          hBar = $('.wrapper--header'),
+          hBarHeight = hBar.outerHeight(),
+          nBar = $('.wrapper--navigation');
+
+      var throttledSticky = $.throttle(100, function() {stickyHeader()});
+      $(window).on('scroll', throttledSticky);
+
+      function stickyHeader() {
+        var st = $(window).scrollTop();
+        if (Math.abs(lastScrollTop - st) <= delta)
+        return;
+        if (st > lastScrollTop && st > hBarHeight){
+          nBar
+            .removeClass('down top')
+            .addClass('up');
+        } else if (st + $(window).height() < $(document).height() && st > hBarHeight) {
+          nBar
+            .removeClass('up')
+            .addClass('down');
+        }
+        else if (st <= 10) {
+          nBar
+            .removeClass('down up')
+            .addClass('top');
+        }
+        lastScrollTop = st;
+      };
+
+   }
+  };
+
+})(jQuery, Drupal, window, document);
